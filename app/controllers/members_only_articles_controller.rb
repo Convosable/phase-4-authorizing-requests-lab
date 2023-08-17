dbe6b195362a79fd68c05601125923f90c768469
Connor Vosberg <connorjv@gmail.com>
2023-08-17 10:46:56 -0700

    commit tree 2a1606683ac36e6c702ee226141a99a485bdbaca

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,9 +1,12 @@
 class MembersOnlyArticlesController < ApplicationController
+
+  before_action :authorize
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+      articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
@@ -15,6 +18,10 @@ class MembersOnlyArticlesController < ApplicationController
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def authorize
+    render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 
 end
